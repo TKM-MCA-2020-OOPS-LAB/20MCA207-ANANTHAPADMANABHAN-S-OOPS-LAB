1e77b7fffbdb5c57751f5ee244786ee7fcfe3d2e
ANANTHAPADMANABHAN S <74697167+ananthapadmanabhans@users.noreply.github.com>
2021-06-03 21:36:32 +0530

    Q1

---
+++ CO 4/Shape/main.java
@@ -0,0 +1,21 @@
+package areaperi;
+
+import java.util.Scanner;
+
+import shapes1.shapes;
+
+
+public class main
+{public static void main(String[] args)
+	{
+	shapes ob=new shapes();
+	ob.rectangle();
+	ob.square();
+	ob.triangle();
+	ob.circle();
+	
+    }
+    
+	
+	}
+	
